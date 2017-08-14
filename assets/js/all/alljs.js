f104cf04188dd30b014df8ef1c1a943ff14f7047
ohjeung <ohjeung@likelion.org>
2017-08-14 11:15:13 +0000

    환경설정 및 뉴스피드

--- assets/js/all/alljs.js
+++ assets/js/all/alljs.js
@@ -7,4 +7,29 @@ $(document).ready(function(e){
     $(".heart_o").toggle();
     $(this).hide();
   });
+  // On/Off 스위치 토글 기능
+  // 알림 설정 스위치
+  $(".onoffswitch-label").click(function(){
+    if($(".switch").attr("disabled")) {
+      $(".switch").removeAttr("disabled");
+    } else {
+      $(".switch").attr("disabled","disabled");
+    }
+  });
+  // 보안 설정 스위치
+  $(".onoffswitch-label2").click(function(){
+    if ($(".switch2").attr("disabled")) {
+      $(".switch2").removeAttr("disabled");
+    } else {
+      $(".switch2").attr("disabled","disabled");
+    }
+  });
+  // On/Off 스위치 토글 기능 end
+});
+
+$('[data-toggle="popover_other_share"]').popover({
+    html: true, 
+	  content: function(e) {
+        return $('#popover-share-other-content').html();
+    }
 });
